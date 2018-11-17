50b77c64215b947592287215fe2e848de3979ff8
bayu <ayahdifa@gmail.com>
2018-11-17 16:13:38 +0700

    tambah file app.js & isi code boilerplate

---
+++ apps.js
@@ -0,0 +1,18 @@
+var express = require("express");
+var path = require("path");
+
+// Inittial app
+var app = express();
+
+// View engine setup
+app.set('views', path.join(__dirname,'views');
+app.set('view engine', 'ejs');
+
+//Setup public folder
+app.use(express.static(path.join(__dirname,'public')));
+
+// Setuo server
+var port = 3000;
+app.listen(port,function(){
+    console.log("Server running on port" + port);
+})
